29011cacd7a9291139fcb330d57b9fb96fdbf7e5
Felipe Garrido <figarrido@uc.cl>
2017-04-29 13:38:04 -0300

    Add views for registration
    Also delete field nickname for users

--- db/schema.rb
+++ db/schema.rb
@@ -46,7 +46,6 @@ ActiveRecord::Schema.define(version: 20170429050720) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.string   "nickname"
     t.integer  "rol"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
