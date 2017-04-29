ab665ad0e59d034ad3237b148eb5dd45842f6a4a
Felipe Garrido <figarrido@uc.cl>
2017-04-29 09:27:44 -0300

    Add devise gem
    Modify schema.rb to use correctly gem devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   enum rol: [:admin, :user, :child]
 	has_many :scores
 	has_many :series
