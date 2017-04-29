e0c26d04f9b9b6422f102c2acd47447dfc039ddf
ctalvarez <ctalvarez@uc.cl>
2017-04-29 14:54:56 -0300

    series scopes, add idiom to series

--- app/models/serie.rb
+++ app/models/serie.rb
@@ -1,6 +1,11 @@
 class Serie < ApplicationRecord
-	has_many :chapters
-	belongs_to :user
-	has_many :genre_serie
-	has_many :genres, through: :genre_serie
+
+  has_many :chapters
+  belongs_to :user
+  has_many :genre_serie
+  has_many :genres, through: :genre_serie
+  scope :by_name, (->(serie_name) {where('name == ?', serie_name )})
+  scope :by_genre, (->(serie_genre) {where('genres == ?', serie_genre )})
+  scope :by_idiom, (->(serie_idiom) {where('idiom == ?', serie_idiom )})
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170429172246) do
+ActiveRecord::Schema.define(version: 20170429175055) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20170429172246) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
+    t.string   "idiom"
     t.index ["user_id"], name: "index_series_on_user_id", using: :btree
   end
 
