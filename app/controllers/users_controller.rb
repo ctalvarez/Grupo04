6c6402168d973d490f22191e9605e750974fea59
David <daruz@uc.cl>
2017-06-21 23:11:43 -0400

    Profile picture and Comments
    Implement profile picture in edit profile, profile picture in profile view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -129,7 +129,7 @@ class UsersController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def user_params
-    params.require(:user).permit(:name, :email, :password)
+    params.require(:user).permit(:name, :email, :password, :image)
   end
 
   def child_params
