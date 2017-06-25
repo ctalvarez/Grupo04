963607fbbfdacd3978a1ea88a3b44baf3b799949
Felipe Garrido <figarrido@uc.cl>
2017-06-25 03:55:42 -0400

    Child's statistics and restrictions

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,9 @@ Rails.application.routes.draw do
   get '/user/:id/new_child', to: 'users#new_child', as: :new_child
   post '/user/:id/children', to: 'users#create_child', as: :children
 
-  get '/user/:id/child/:child_id', to: 'users#statistics', as: :statistics
+  get '/user/:id/child/:child_id', to: 'users#child', as: :child
+  get '/user/:id/child/:child_id/edit', to: 'users#edit_child', as: :child_edit
+  patch '/user/:id/child/:child_id', to: 'users#update_child'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
