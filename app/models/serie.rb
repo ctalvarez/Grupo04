53598001afec4282ff385723c72233cdc9f126e0
David <daruz@uc.cl>
2017-06-25 23:07:20 -0400

    Add directors in series create forms
    Also change series show and more

--- app/models/serie.rb
+++ app/models/serie.rb
@@ -9,8 +9,8 @@ class Serie < ApplicationRecord
   has_many :actor_series, dependent: :destroy
   has_many :actors, through: :actor_series, source: :actor
   has_many :sessions, dependent: :destroy
-  has_one :director_series, dependent: :destroy
-  has_one :director, through: :director_series, source: :director
+  has_many :director_series, dependent: :destroy
+  has_many :director, through: :director_series, source: :director
   has_many :news, dependent: :nullify
 
   belongs_to :user
